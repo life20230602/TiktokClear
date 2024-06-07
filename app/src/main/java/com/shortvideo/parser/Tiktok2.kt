d3d68f93c3ce87e8d028f1c55957409190645c9d
life <>
2024-06-07 20:07:38 +0800

    111

--- app/src/main/java/com/shortvideo/parser/Tiktok2.kt
+++ app/src/main/java/com/shortvideo/parser/Tiktok2.kt
@@ -1,26 +1,36 @@
 package com.shortvideo.parser
 
-import android.net.Uri
+import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.FormBody
+import okhttp3.MediaType
+import okhttp3.RequestBody
 import org.json.JSONObject
+import java.util.regex.Pattern
 
 
 class Tiktok2 : ShortVideoParser() {
     override suspend fun getDownloadUrl(url: String): String = withContext(Dispatchers.IO) {
-        createRequest("https://api.twitterpicker.com/tiktok/mediav2?id=${Uri.parse(url).lastPathSegment}")?.let {
+        createRequest("https://tiksave.io/api/ajaxSearch")?.let {
             try {
-                val request = it.header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-                    .header("Referer","https://tiktokdownloader.com/")
-                    .get().build()
+                val requestBody = FormBody.Builder().add("q",url);
+                val request = it.header("User-Agent",
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+                    .header("Referer","https://tiksave.io")
+                    .post(requestBody.build()).build()
                 val response = sClient.newCall(request).execute()
                 if (response.isSuccessful) {
                     response.body()?.let { body ->
-                        val resultJsonObject = JSONObject(body.string())
-                        val mediasJsonObject = resultJsonObject.getJSONObject("video_no_watermark")
-                        val url = mediasJsonObject.getString("url")
-                        return@withContext url
+                        val resultJsonObject = JSONObject(body.string()).getString("data")
+                        val pattern = Pattern.compile("[a-zA-z]+://[^\\s]*")
+                        val matcher = pattern.matcher(resultJsonObject)
+                        var last = "";
+                        while (matcher.find()){
+                            last = matcher.group();
+                        }
+                        Log.e("====", "getDownloadUrl: "+last )
+                        return@withContext last
                     }
                 }
             }catch (e : Exception){
